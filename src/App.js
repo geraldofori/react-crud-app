e52b3211866465f4153292d250d47fb49dc8946d
gerald <oforige@gmail.com>
2022-06-29 10:57:25 +0000

    added routes to the various components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Home } from './components/Home';
+import {AddUser} from './components/AddUser';
+import {EditUser} from './components/EditUser';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Router>
+      <h1>Nav</h1>
+        <Routes>
+          <Route path="/" component={Home}/>
+          <Route path="/add" component={AddUser}/>
+          <Route path="/edit" component={EditUser}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
